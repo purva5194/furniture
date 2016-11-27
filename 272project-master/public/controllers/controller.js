af0ef0f63864533e3b2288439079e07b02965537
purva5194 <purvaben.patel@sjsu.edu>
2016-11-26 22:22:27 -0800

    User module

---
+++ 272project-master/public/controllers/controller.js
@@ -0,0 +1,375 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+var Item_name="";
+var Item_manu="";
+var Item_price="";
+
+myApp.config(function($routeProvider){
+  $routeProvider
+  
+  .when('/login',{
+    templateUrl : '/login.html',
+    controller : 'RegCtrl'
+  })
+
+  .when('/home',{
+    templateUrl : '/home.html',
+    controller : 'HomeCtrl'
+	
+  })
+  
+  .when('/contact',{
+    templateUrl : '/contact.html',
+    controller : 'ContactCtrl'
+  })
+  
+  .when('/livingroom',{
+    templateUrl : '/LivingRoom.html',
+    controller : 'LivingRoomCtrl'
+  })
+  
+  .when('/bedroom',{
+    templateUrl : '/Bedroom.html',
+    controller : 'BedroomCtrl'
+  })
+  
+  .when('/diningroom',{
+    templateUrl : '/Dining Room.html',
+    controller : 'DiningroomCtrl'
+  })
+  
+  .when('/studyroom',{
+    templateUrl : '/StudyRoom.html',
+    controller : 'StudyroomCtrl'
+  })
+  
+  .when('/userdetail',{
+    templateUrl : '/UserDetail.html',
+    controller : 'UserCtrl'
+  })
+  
+  .when('/productdetail',{
+    templateUrl : '/product_detail.html',
+    controller : 'ProductCtrl'
+  })
+  
+  .when('/cart',{
+    templateUrl : '/cart.html',
+    controller : 'CartCtrl'
+  })
+  
+  .when('/sale',{
+    templateUrl : '/sale.html',
+    controller : 'SaleCtrl'
+  })
+  .otherwise({redirectTo: '/home'});
+});
+
+myApp.factory('Scopes', function ($rootScope) {
+    var mem = {};
+ 
+    return {
+        store: function (key, value) {
+            mem[key] = value;
+        },
+        get: function (key) {
+            return mem[key];
+        }
+    };
+});
+/**
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+
+
+var refresh = function() {
+  $http.get('/contactlist').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.contactlist = response;
+    $scope.contact = "";
+  });
+};
+
+refresh();
+
+$scope.addContact = function() {
+  console.log($scope.contact);
+  $http.post('/contactlist', $scope.contact).success(function(response) {
+    console.log(response);
+    refresh();
+  });
+};
+
+$scope.remove = function(id) {
+  console.log(id);
+  $http.delete('/contactlist/' + id).success(function(response) {
+    refresh();
+  });
+};
+
+$scope.edit = function(id) {
+  console.log(id);
+  $http.get('/contactlist/' + id).success(function(response) {
+    $scope.contact = response;
+  });
+};  
+
+$scope.update = function() {
+  console.log($scope.contact._id);
+  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+    refresh();
+  })
+};
+
+$scope.deselect = function() {
+  $scope.contact = "";
+}
+
+}]);﻿
+**/
+
+//registration
+myApp.controller('RegCtrl', ['$scope', '$http', '$location', '$rootScope', '$window', function($scope, $http, $location, $rootScope, $window) {
+    console.log("Hello World from controller");
+
+	//destroy session
+	$http.get('/sessiondestroy').success(function(response){
+		console.log("session destroyed");
+		$rootScope.loginlogout = "Login";
+		$rootScope.username="";		//no use logged in
+	});
+
+	// insert customer details
+	$scope.addCustomer = function() {
+	  console.log($scope.customer);
+	  $http.post('/customerlist', $scope.customer).success(function(response) {
+		console.log(response);
+		$window.alert("Registration Successful..!!")
+	  });
+	};
+
+	//check username and password
+	$scope.checkCustomer = function(name,password) {
+		console.log($scope.login);
+	  $http.get('/customerlist/'+ name +'/'+password, $scope.login).success(function(response) {
+		console.log("I got the data I requested");
+		console.log(response);
+		
+		$scope.status="Wrong username or Password. Try Again!!";
+		
+		if(response.toString() == "successful"){
+			$scope.status="";
+			$location.path('/home');
+	}
+  });
+};
+
+}]);﻿
+
+//home
+myApp.controller('HomeCtrl', ['$scope', '$http', '$window', '$rootScope', function($scope, $http, $window, $rootScope) {
+    console.log("Hello World from Home controller");
+	
+	console.log('onload');
+	
+	//session checking
+	$http.get('/sessioncheck').success(function(response) {
+    console.log("I got the data I requested");
+    console.log(response);
+	
+	if(response.toString() == 'not exist'){
+		$rootScope.loginlogout='Login';
+		$rootScope.username="";
+	}
+	else{
+		$rootScope.loginlogout='Logout';
+		$rootScope.username=response.toString();
+	}
+
+  });
+		
+	$scope.detail= function(name,manu,price)
+	{
+		Item_name=name;
+		Item_manu=manu;
+		Item_price=price;
+	};
+
+
+}]);﻿
+
+//living room
+myApp.controller('LivingRoomCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    console.log("Hello World from living room  controller");
+	
+	$scope.detail= function(name,manu,price)
+	{
+		Item_name=name;
+		Item_manu=manu;
+		Item_price=price;
+	};
+
+}]);﻿
+
+//bed room
+myApp.controller('BedroomCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    console.log("Hello World from  Bedroom  controller");
+	
+	$scope.detail= function(name,manu,price)
+	{
+		Item_name=name;
+		Item_manu=manu;
+		Item_price=price;
+	};
+
+}]);﻿
+
+//dining room
+myApp.controller('DiningroomCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    console.log("Hello World from  Dining room  controller");
+	
+	$scope.detail= function(name,manu,price)
+	{
+		Item_name=name;
+		Item_manu=manu;
+		Item_price=price;
+	};
+
+
+}]);﻿
+
+//study room
+myApp.controller('StudyroomCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    console.log("Hello World from  study room  controller");
+	
+	$scope.detail= function(name,manu,price)
+	{
+		Item_name=name;
+		Item_manu=manu;
+		Item_price=price;
+	};
+
+}]);
+
+//USer Detail
+myApp.controller('UserCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+    console.log("Hello World from  User  controller");
+	console.log($rootScope.username);
+	var temp_username = $rootScope.username;
+	
+	$http.get('/customerlist/'+ temp_username).success(function(response) {
+    console.log("I got the user info");
+    //console.log(response);
+	
+	$scope.u_name=response.name;
+	$scope.u_address=response.address;
+	$scope.u_city=response.city;
+	$scope.u_state=response.state;
+	$scope.u_zip=response.zip;
+	$scope.u_phone=response.phonenumber;
+	$scope.u_email=response.email;
+  });
+}]);
+
+//product detail
+myApp.controller('ProductCtrl', ['$scope', '$http', '$window', '$location', '$rootScope', function($scope, $http, $window, $location, $rootScope) {
+    console.log("Hello World from  product  controller");
+	
+	console.log(Item_name, Item_manu, Item_price);
+	
+	//fetch data from itemlist
+	$http.get('/customerlist/'+ Item_name +'/'+ Item_manu +'/'+ Item_price).success(function(response) {
+    console.log("I got the item info");
+    console.log(response);
+	
+	$scope.Item=response;;
+	
+  });
+  
+  // add item into user's cart 
+  $scope.addToCart = function()
+	{
+		console.log($scope.Item);
+		//check whether the user is logged in or not
+		if($rootScope.username.toString() == ""){
+			//if not
+			$window.alert("Please Sign in to add item in your cart..!")
+			$location.path('/login');
+		}
+		else{
+			//if yes
+			console.log($rootScope.username);
+			$http.post('/cartlist/' + $rootScope.username, $scope.Item).success(function(response) {
+			console.log("data entry done..!");
+			console.log(response);
+			});
+		
+			$window.alert('Product Added to Cart Successfully..!!');
+			$location.path('/cart');
+		}	
+	};
+
+}]);
+
+//cart
+myApp.controller('CartCtrl', ['$scope', '$http', '$window', '$rootScope', '$location', function($scope, $http, $window, $rootScope, $location) {
+    console.log("Hello World from Cart controller");
+	console.log($rootScope.username);
+	
+	var refresh = function() {
+		if($rootScope.username.toString() == ""){
+			$window.alert("Please Sign in to see items you may have already added to your cart..!")
+			$location.path('/login');
+		}
+		else{
+			$http.get('/cartlist/'+ $rootScope.username).success(function(response) {
+			console.log("I got the item info");
+			console.log(response);
+			var total=0;
+			
+			for(var i=0; i<response.length; i++)
+			{
+				response[i].itemPrice=(response[i].itemPrice).substr(1);
+				total=total + parseFloat(response[i].itemPrice)*parseFloat(response[i].itemCustQnty);
+			}
+			$scope.itemlist=response;
+			console.log(total);
+			$scope.total=total;
+			$scope.vat = (parseFloat(total) * 8.5) /100;
+			$scope.totalWithTax = parseFloat($scope.vat) + parseInt($scope.total) + 2;
+			
+			});
+		}
+	}
+	refresh();
+	
+	// delete item from user's cart 
+	$scope.deleteItem = function(cart_id)
+	{
+		console.log("delete request");
+		console.log(cart_id);
+		$http.delete('/cartlist/' + cart_id).success(function(response) {
+		refresh();
+		});
+	}
+	
+	//cancel
+	$scope.cancel = function()
+	{
+		$location.path('/home');
+	}
+}]);﻿
+
+//sale
+myApp.controller('SaleCtrl', ['$scope', '$http', '$window', '$rootScope', function($scope, $http, $window, $rootScope) {
+    console.log("Hello World from Sale controller");
+	
+		
+	$scope.itemDetail= function(name,manu,price)
+	{
+		Item_name=name;
+		Item_manu=manu;
+		Item_price=price;
+	};
+
+
+}]);﻿
---
+++ 272project-master/server.js
@@ -0,0 +1,214 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+//var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
+var db1 = mongojs('user1:user1@ds145677.mlab.com:45677/sampleapp',['customerlist']);
+//var cookie = require('cookie');
+var session = require('express-session');
+var mongoose = require('mongoose');
+
+
+
+var sess;
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: false,
+    resave: false,
+    HttpOnly: false, 
+    
+}));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+/**
+app.get('/contactlist', function (req, res) {
+  console.log('I received a GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+**/
+
+//customer registration
+app.post('/customerlist', function (req, res) {
+  console.log(req.body);
+  db1.customerlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+//whether the user enter correct password username and password
+app.get('/customerlist/:name/:password', function (req, res) {
+	
+  console.log('I received a GET request');
+
+  db1.customerlist.find({},{"username":1,"password":1,_id:0}).toArray(function (err, docs) {
+	console.log(docs);
+	//console.log(docs[0].username);
+	flag = "unsuccessful";
+	
+	for (i=0; i<docs.length; i++)
+	{
+		if(docs[i].username.toString() == req.params.name.toString() & docs[i].password.toString() == req.params.password.toString())
+		{
+			console.log('successful');
+			flag = "successful"
+			break;
+		}
+	}
+	
+	if(flag == "successful")
+	{
+		/*res.setHeader('Set-Cookie', cookie.serialize('username', String(req.params.name), {
+			httpOnly: true,
+		}));*/
+		sess = req.session;
+		sess.username=req.params.name;
+		res.send("successful");
+	}
+	else
+	{
+		res.send("unsuccessful");
+	}
+	
+  });
+});
+
+//session check
+app.get('/sessioncheck', function (req, res) {
+	
+  console.log('I received a session check request');
+  sess = req.session;
+	
+  if(sess.username)
+  {
+	  res.send(sess.username);
+  }
+  else
+  {
+	  res.send("not exist");
+  }
+	
+});
+
+//session destroy
+app.get('/sessiondestroy', function (req, res) {
+	
+  console.log('I received a session destroy request');
+  sess = req.session;
+  sess.destroy(function(err) {
+        if(err){
+             console.log('Error destroying session');
+			 res.send("not done");
+        }else{
+            console.log('Session destroy successfully');
+			res.send("done");
+        }
+    });
+	
+});
+
+//User Info
+//for customer detail page
+app.get('/customerlist/:name', function (req, res) {
+
+  console.log('I received a user info request');
+
+  db1.customerlist.findOne({"username" : req.params.name.toString()}, function (err, docs) {
+	console.log(docs);
+	res.json(docs);
+	
+  });
+});
+
+//item info for product_detail page
+app.get('/customerlist/:name/:manu/:price', function (req, res) {
+
+  console.log('I received a item info request');
+
+  db1.itemlist.findOne({"itemName" : req.params.name.toString(), "itemManufacturer" : req.params.manu.toString(), "itemPrice" : req.params.price.toString()}, function (err, docs) {
+	console.log(docs);
+	res.json(docs);
+	
+  });
+});
+
+//insert item into cartlist
+app.post('/cartlist/:username', function (req, res) {
+  console.log(req.body.itemName);
+  var data = {"userName":req.params.username.toString(), 
+							"itemName":req.body.itemName.toString(), 
+							"itemPrice":req.body.itemPrice.toString(), 
+							"itemManufacturer":req.body.itemManufacturer.toString(), 
+							"itemPath":req.body.itemPath.toString(), 
+							"itemCustQnty":req.body.custQnty.toString()};
+							
+  db1.cartlist.insert(data, function(err, doc) {
+	  console.log("data -> cart");
+    res.json(doc);
+  });
+});
+
+//cart info for user based on username
+app.get('/cartlist/:name', function (req, res) {
+
+  console.log('I received a cart info request');
+
+  db1.cartlist.find({"userName" : req.params.name.toString()}, function (err, docs) {
+	console.log(docs);
+	res.json(docs);
+	
+  });
+});
+
+// delete item from cart based on _id
+app.delete('/cartlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db1.cartlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
