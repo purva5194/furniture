3573c54cecf1972d1e8e6de26b09c48f97077228
purva5194 <purvaben.patel@sjsu.edu>
2016-11-28 21:09:30 -0800

    Update server.js

--- 272project-master/server.js
+++ 272project-master/server.js
@@ -210,5 +210,6 @@ app.delete('/cartlist/:id', function (req, res) {
   });
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
